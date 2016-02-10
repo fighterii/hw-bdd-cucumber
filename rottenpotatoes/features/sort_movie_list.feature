e70c8544e9ecb78a40ac0299a1a517aa1a7de668
fighterii <fighterii.kb@googlemail.com>
2016-02-10 10:40:45 +0000

    trying heroku

--- rottenpotatoes/features/sort_movie_list.feature
+++ rottenpotatoes/features/sort_movie_list.feature
@@ -19,7 +19,7 @@ Background: movies have been added to database
   | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
   | Chicken Run             | G      | 21-Jun-2000  |
 
-  And I am on the RottenPotatoes home page
+  And I am on the home page
 
 Scenario: sort movies alphabetically
   When I follow "Movie Title"
--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -13,8 +13,11 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-    when /^the movies page$/ then '/movies'
+    when /^the home\s?page$/
+      ''
+      
+    when /^the edit page for "(.*)"$/i
+      edit_movie_path(Movie.find_by title: $1)
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
